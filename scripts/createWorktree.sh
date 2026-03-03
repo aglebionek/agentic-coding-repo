e252a2582f9c160a5ff0bd9d3f2f38aa26ebf82f
Aleksander Glebionek <aglebionek@gmail.com>
2026-03-03 20:23:11 +0100

    Refactor comments in createWorktree.sh
    Updated comments for clarity and removed specific paths.

--- scripts/createWorktree.sh
+++ scripts/createWorktree.sh
@@ -1,16 +1,11 @@
 ${0%/*}/checkIfAlreadyInWorktree.sh
 
-# From the main repo root (/home/aglebionek/work/instago/app)
+# From the main repo root 
 BRANCH_NAME=$1
 git worktree add "../worktrees/${BRANCH_NAME}" -b "${BRANCH_NAME}" develop
 cd "../worktrees/${BRANCH_NAME}"
 
-cp /home/aglebionek/work/instago/app/server/.env server/.env
-cp /home/aglebionek/work/instago/app/client/.env client/.env
-ln -s /home/aglebionek/work/instago/app/server/db server
-ln -s /home/aglebionek/work/instago/app/client/node_modules client/node_modules
-ln -s /home/aglebionek/work/instago/app/server/node_modules server/node_modules
-ln -s /home/aglebionek/work/instago/app/.aglebionek .aglebionek
+# copy or symlink anything you might need in the worktree, like .env files or node_modules
 
 cat > .worktree-session <<EOF
 path-to-worktree: ../worktrees/${BRANCH_NAME}
